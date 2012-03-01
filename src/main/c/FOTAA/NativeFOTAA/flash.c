b6655a9302fd9a65c95a12f7cb8bf71340c46247
JeD <jedartois@gmail.com>
2012-03-01 12:46:59 +0100

    no msg

--- src/main/c/FOTAA/NativeFOTAA/flash.c
+++ src/main/c/FOTAA/NativeFOTAA/flash.c
@@ -438,7 +438,7 @@ void *flash_firmware(Target *infos)
             FlashEvent(-2);
         }
 
-        if(ack1 == 84 && (ack2 == 254 || ack2 == 255) && ack3 == 255)
+        if(ack1 == 'T' && ack2 == 'F')
         {
             FlashEvent(-38);
         }
